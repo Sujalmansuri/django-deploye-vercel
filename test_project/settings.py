ada86f10ae10f6b8de6c20f1681b52b05ae83af6
Sujalmansuri <sujalmansuri42@gmail.com>
2025-04-29 15:48:59 +0530

    Updated project with recent changes

--- test_project/settings.py
+++ test_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'social_django', 
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
