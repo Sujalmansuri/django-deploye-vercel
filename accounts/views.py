1312263b3c47904a459a296f3ffc89b228a18e23
Sujal Mansuri <115333347+Sujalmansuri@users.noreply.github.com>
2025-05-04 20:19:41 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -110,8 +110,7 @@ from datetime import timedelta
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=255)
     file = forms.FileField()
-    
-@login_required(login_url='login')  # Force login if not authenticated
+
 def upload(request):
     if request.method == "POST" and request.FILES.get("file"):
         form = UploadFileForm(request.POST, request.FILES)
