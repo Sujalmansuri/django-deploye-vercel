820b3134945c8ee84d2cfd0eb71f0d24ae56e88b
Sujalmansuri <sujalmansuri42@gmail.com>
2025-05-04 09:43:02 +0530

    Update Database

--- test_project/settings.py
+++ test_project/settings.py
@@ -82,12 +82,19 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True  # Ensures SSL is used
+    )
 }
+
 
 
 # Password validation
