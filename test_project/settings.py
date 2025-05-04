49763101abf49e9c002dd7c0b707083d15a0fbd3
Sujalmansuri <sujalmansuri42@gmail.com>
2025-05-04 11:10:32 +0530

    Required changes

--- test_project/settings.py
+++ test_project/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),   
+        'PORT': os.getenv('DB_PORT', '6543'),   
         'OPTIONS': {
             'sslmode': 'require',  # ✅ Enforces SSL for Supabase
         },
