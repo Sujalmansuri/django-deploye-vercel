abdf135d7eb6238b505858df99a1079a92797f45
Sujalmansuri <sujalmansuri42@gmail.com>
2025-05-05 23:27:29 +0530

    new changes in settting and viewss

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,7 @@
-# forms.py
 from django import forms
 from .models import UploadedFile
 
 class UploadFileForm(forms.ModelForm):
-    file = forms.FileField(required=True)
-
     class Meta:
         model = UploadedFile
-        fields = ['title', 'file']
+        fields = ['title', 'file_url']  # Adjust fields as necessary
