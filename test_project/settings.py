75158bc69473b134eecac53cc438838c1ac98031
Sujal Mansuri <sujalmansuri42@gmail.com>
2026-05-17 10:23:46 +0530

    Fixed Google OAuth

--- test_project/settings.py
+++ test_project/settings.py
@@ -168,8 +168,8 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load .env variables
 
-"client_id": os.getenv("GOOGLE_CLIENT_ID")
-"client_secret": os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
 
 import json
